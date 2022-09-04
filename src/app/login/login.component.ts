0730aa6f39104b45b2fd4f78a024b56f0d031276
DeyvesDion <deyvesdion@gmail.com>
2022-09-04 19:06:49 +0200

    last part

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({ 
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
   }
+  login(){
+    this.http.get<any>("http://localhost:3000/signup")
+      .subscribe(res => {
+      // match email and password
+        const user = res.find((a: any) => {
+          return a.email === this.loginForm.value.email
+            &&
+            a.password === this.loginForm.value.password;
+        })
+        //condition for login
+        if (user) {
+          alert('sucessfully logged');
+          this.loginForm.reset();
+          this.router.navigate(['student'])
+        }
+        else {
+          alert('user not found credentials');
+        }
+      },
+      err=>(alert("something went wrong")))
+    }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+signUpForm!: FormGroup
+  constructor(private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      name:['', Validators.required],
+      mobile:['', Validators.required],
+      email:['', Validators.required],
+      password:['', Validators.required]
+    })
+  }
+
+  // Creation
+
+  signup() {
+    this.http.post<any>('http://localhost:3000/signup/',this.signUpForm.value)
+      .subscribe(res => {
+        alert("Student registred sucessfully");
+        this.signUpForm.reset();
+        this.router.navigate(['/login']);
+      },
+        err => {
+        alert("Something went wrong");
+      })
   }
 
 }
